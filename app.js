98afe08529795ad6ba76318d6c1d3050ac6af87c
Jenny Murphy <jmrigg02@gmail.com>
2019-04-19 12:40:28 -0400

    updated main page and added classes

---
+++ app.js
@@ -0,0 +1,13 @@
+/* Collapsible Class Buttons */
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i ++) {
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block"){
+            content.style.dispaly = "none";
+        } else {content.style.display = "block";}
+    })
+}
